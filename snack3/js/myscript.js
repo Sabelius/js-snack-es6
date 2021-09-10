3301ab4479225302db55cb3fb032d8ca96041837
Sabelius <sbebby92@virgilio.it>
2021-09-10 17:12:40 +0200

    versione finale 2.5

--- snack3/js/myscript.js
+++ snack3/js/myscript.js
@@ -1,16 +1,22 @@
 const numbers = [11, 22, 33, 74, 85, 96, 27, 38, 49, 50, 5, 7, 8, 12, 45, 66];
 console.log(numbers);
 
+// function myFun(array, a, b) {
+  //   const fewNumbers = array.filter((element, index) => {
+  //     if (index >= a && index <= b) {
+  //       return element;
+  //     }
+  //   });
+  //   return fewNumbers;
+    
+  // }
 
 function myFun(array, a, b) {
-  const fewNumbers = array.filter((element, index) => {
-    if (index >= a && index <= b) {
-      return element;
-    }
-  });
+  const fewNumbers = array.filter((element, index) => index >= a && index <= b);
   return fewNumbers;
-  
+
 }
+
 const fewNumbers = myFun(numbers, 4, 12);
 console.log(fewNumbers);
 
